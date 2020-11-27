3f8226c4cf7beb2643df5ca5814bea51c0a61cf1
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-27 06:42:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,9 +57,10 @@ function insertNodeAt(index, address, list, collection)
 
   console.log(index, address, list)
   let prevNode = nodeAt(index-1,list,collection)
-  
+  let prevAddress = addressAt(index-1,list,collection)
   collection[address.next] = prevNode.next
-  collection[prevNode.next] = address
+  prevNode.next = address
+  collection[prevAddress] = prevNode
   
   
   console.log(collection)
